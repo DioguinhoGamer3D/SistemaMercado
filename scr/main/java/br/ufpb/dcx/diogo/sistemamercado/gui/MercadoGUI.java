27d81b14adadbbc9e4b3eb0e7447d07a96e2ac0c
Diogo <diogo.silva@dcx.ufpbr.br>
2025-03-01 22:51:45 -0300

    um pequeno detalhe que foi feito, falta adiconar o botão de salvar e um de sair, fora isso quase finalizado

--- scr/main/java/br/ufpb/dcx/diogo/sistemamercado/gui/MercadoGUI.java
+++ scr/main/java/br/ufpb/dcx/diogo/sistemamercado/gui/MercadoGUI.java
@@ -20,39 +20,39 @@ public class MercadoGUI extends JFrame {
         JButton botaoCadastrar, botaoPesquisar, botaoRemover, botaoAlterar;
         Estoque estoque = new GerenciaEstoque();
 
-        public MercadoGUI() {
-            setTitle("Sistema Gerencia de Mercado");
-            setSize(800, 600);
-            setLocation(150, 150);
-            setResizable(false);
-            getContentPane().setBackground(new Color(254, 255, 239));
-            linha1 = new JLabel("Sistema para gerenciar um mercado", JLabel.CENTER);
-            linha1.setForeground(Color.red);
-            linha1.setFont(new Font("Serif", Font.BOLD, 24));
-            linha2 = new JLabel(mercadoImg, JLabel.CENTER);
-            botaoCadastrar = new JButton("Cadastrar produto", cadImg);
-            botaoCadastrar.setBackground(new Color(254, 255, 239));
-            botaoCadastrar.addActionListener(new MercadoCadController(estoque,this));
-            botaoRemover = new JButton("Remover produto",removeImg);
-            botaoRemover.setBackground(new Color(254, 255, 239));
-            botaoRemover.addActionListener(new MercadoRemoveController(estoque,this));
-            botaoPesquisar = new JButton("Pesquisar produto", pesqImg);
-            botaoPesquisar.setBackground(new Color(254, 255, 239));
-            botaoPesquisar.addActionListener(new MercadoSeachController(estoque,this));
-            botaoAlterar = new JButton("Alterar Produto", altImg);
-            botaoAlterar.setBackground(new Color(254, 255, 239));
-            botaoAlterar.addActionListener(new MercadoChangeController(estoque,this));
-            getContentPane().setLayout(new GridLayout(3,2));
-            getContentPane().add(linha1);
-            getContentPane().add(linha2);
-            getContentPane().add(botaoCadastrar);
-            getContentPane().add(botaoRemover);
-            getContentPane().add(botaoPesquisar);
-            getContentPane().add(botaoAlterar);
-        }
-            public static void main(String [] args){
+    public MercadoGUI() {
+        setTitle("Sistema Gerencia de Mercado");
+        setSize(800, 600);
+        setLocation(150, 150);
+        setResizable(false);
+        getContentPane().setBackground(new Color(254, 255, 239));
+        linha1 = new JLabel("Sistema para gerenciar um mercado", JLabel.CENTER);
+        linha1.setForeground(Color.red);
+        linha1.setFont(new Font("Serif", Font.BOLD, 24));
+        linha2 = new JLabel(mercadoImg, JLabel.CENTER);
+        botaoCadastrar = new JButton("Cadastrar produto", cadImg);
+        botaoCadastrar.setBackground(new Color(254, 255, 239));
+        botaoCadastrar.addActionListener(new MercadoCadController(estoque,this));
+        botaoRemover = new JButton("Remover produto",removeImg);
+        botaoRemover.setBackground(new Color(254, 255, 239));
+        botaoRemover.addActionListener(new MercadoRemoveController(estoque,this));
+        botaoPesquisar = new JButton("Pesquisar produto", pesqImg);
+        botaoPesquisar.setBackground(new Color(254, 255, 239));
+        botaoPesquisar.addActionListener(new MercadoSeachController(estoque,this));
+        botaoAlterar = new JButton("Alterar Produto", altImg);
+        botaoAlterar.setBackground(new Color(254, 255, 239));
+        botaoAlterar.addActionListener(new MercadoChangeController(estoque,this));
+        getContentPane().setLayout(new GridLayout(3,2));
+        getContentPane().add(linha1);
+        getContentPane().add(linha2);
+        getContentPane().add(botaoCadastrar);
+        getContentPane().add(botaoRemover);
+        getContentPane().add(botaoPesquisar);
+        getContentPane().add(botaoAlterar);
+    }
+    public static void main(String [] args){
                 JFrame janela = new MercadoGUI();
                 janela.setVisible(true);
                 janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            }
+    }
 }
